a7ac0f81c47b0f71efc274cd2d361840e480b5df
ustamhok <ustamhok8815@gmail.com>
2021-08-27 20:37:23 +0900

    deviseに関するビューファイルの配置

--- db/migrate/20210826123442_devise_create_users.rb
+++ db/migrate/20210826123442_devise_create_users.rb
@@ -4,10 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :name,               null: false
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -43,3 +43,4 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     # add_index :users, :unlock_token,         unique: true
   end
 end
+
