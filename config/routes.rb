7891389213c4dbfac03da777b18f38c7df7a2e1e
ustamhok <ustamhok8815@gmail.com>
2021-08-29 11:33:58 +0900

    プルダウンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'messages/index'
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms, only: [:new, :create]
 end
